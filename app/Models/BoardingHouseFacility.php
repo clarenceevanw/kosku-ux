d60a55dc689912d3e9b70f233c156ef99b296013
clarenceevanw <clarenceevan0907@gmail.com>
2026-05-20 10:32:09 +0700

    feat: define table in pivot model

--- app/Models/BoardingHouseFacility.php
+++ app/Models/BoardingHouseFacility.php
@@ -8,10 +8,12 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Guarded;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Attributes\Table;
 
 #[Fillable(['boarding_house_id', 'facility_id'])]
 #[Guarded(['id'])]
 #[Hidden([])]
+#[Table('boarding_house_facility')]
 class BoardingHouseFacility extends Pivot
 {
     use HasUuids;
--- app/Models/RoomFacility.php
+++ app/Models/RoomFacility.php
@@ -8,10 +8,12 @@ use Illuminate\Database\Eloquent\Relations\Pivot;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Guarded;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Attributes\Table;
 
 #[Fillable(['room_id', 'facility_id'])]
 #[Guarded(['id'])]
 #[Hidden([])]
+#[Table('room_facility')]
 class RoomFacility extends Pivot
 {
     use HasUuids;
